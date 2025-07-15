5de2610135bdfab4f4495b7d85c3225c916a2ef4
luke <125353302+lukeduck321@users.noreply.github.com>
2025-07-15 15:56:28 +0930

    Add files via upload

--- client side.py
+++ client side.py
@@ -1,17 +1,56 @@
 import socket
+from time import sleep
 
-def start(server_ip):
+
+host = "0.0.0.0"
+port = 12345
+message = str()
+
+def send(server_ip, message):
     port = 12345
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_ip, port))
 
-    message = "hello from client"
+ 
     client_socket.send(message.encode())
     
-    data = client_socket.recv(1024)
-    print("from server:",data.decode())
-
+    response = b""
+    while True:
+        part = client_socket.recv(1024)
+        if not part:
+            break
+        response += part
+    
+    print("From server:", response.decode())
     client_socket.close()
 
-start("192.168.1.135")
+def rec():
+    global message
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen()  
+    print("server listening on", host,port)
+    conn, addr = server_socket.accept()
+    print(addr,"connected")
+
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        message = data.decode()
+        conn.send(b"message received")
+
+    conn.close()
+    server_socket.close()
+
+def establish():
+    global message
+    message = "//es"
+    send("192.168.1.135", message)
+    rec()
+
+establish()
+while True:
+    message = input("type message you want to send: ")
+    send("192.168.1.135", message)
--- server side.py
+++ server side.py
@@ -1,25 +1,66 @@
 import socket
+from time import sleep
 
 host = "0.0.0.0"
 port = 12345
+message = str()
 
-def start():
+def rec():
+    global message
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen()
-
+    server_socket.listen()  
     print("server listening on", host,port)
     conn, addr = server_socket.accept()
-    print(addr,"connected")
-    
+
     while True:
+        conn, addr = server_socket.accept()
+        print(addr, "connected")
+
         data = conn.recv(1024)
         if not data:
-            break
-        print("recived:",data.decode())
-        conn.send(b"message received")
-    
-    conn.close()
-    server_socket.close()
+            conn.close()
+            continue
 
-start()
+        message = data.encode()
+        print("recived:", message)
+
+        if message.strip() == "//es":
+            try:
+                f = open("log.txt", "r")
+                file_data = f.read()
+                conn.sendall(file_data.encode())
+            except FileNotFoundError:
+                conn.sendall(b"file not found")
+        else:
+            conn.sendall(b"message recived")
+
+        conn.close()
+
+def send():
+    global message
+    port = 12345
+    server_ip = "255.255.255.255"
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    sock.sendto(message.encode(), (server_ip, port))
+    
+    data = sock.recv(1024)
+    print(data.decode())
+
+    sock.close()
+
+    if message == "//es":
+        message = str()
+
+while True:
+    sleep(1)
+    if message == "//es":
+        file = open("log.txt", "r")
+        message = file.read()
+        print(message)
+        send()
+        rec()
+    rec()
