6f7c3f7bc88fb5d42b5645ae3b538ce9a2d0fd9f
luke <125353302+lukeduck321@users.noreply.github.com>
2025-08-29 20:41:42 +0930

    Add files via upload

--- new/clientside-gui.py
+++ new/clientside-gui.py
@@ -1,37 +1,88 @@
 import socket
-from time import sleep
+import threading
+from tkinter import *
 
 host = "0.0.0.0"
 port = 12345
+server_ip = "192.168.8.112"
 message = ""
+
+#gui setup
+window = Tk()
+guiout = StringVar()
+
+
+def send(server_ip, message):
+    port = 12345
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect((server_ip, port))
+        if message == "//es":
+            pass
+        else:
+            message = "Ducky: " + message
+        client_socket.send(message.encode())
+        response = b""
+        while True:
+            part = client_socket.recv(1024)
+            if not part:
+                break
+            response += part
+        print("From server:\n", response.decode())
+    except Exception as e:
+        print("Send error:", e)
+    finally:
+        client_socket.close()
 
 def rec():
     global message
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen()
-    print("Server listening on", host, port)
-
+    print("Client receiver listening on", host, port)
     while True:
         conn, addr = server_socket.accept()
         print(addr, "connected")
-        data = conn.recv(1024)
-        if not data:
-            conn.close()
-            continue
-
-        message = data.decode()
-        print("Received:", message)
-
-        if message.strip() == "//es":
-            try:
-                with open("log.txt", "r") as f:
-                    file_data = f.read()
-                    conn.sendall(file_data.encode())
-            except FileNotFoundError:
-                conn.sendall(b"file not found")
-        else:
-            conn.sendall(b"message received")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            message = data.decode()
+            print("Received:", message)
+            conn.send(b"message received")
         conn.close()
 
-rec()
+# Run receiver in background
+threading.Thread(target=rec, daemon=True).start()
+
+# Send establish signal
+"""send(server_ip, "//es")
+
+# Main loop
+while True:
+    message = input("Type message you want to send: ")
+    send(server_ip, message)
+"""
+
+window.geometry("400x600+1520+480")
+
+#enrties
+ent1 = Entry(window, textvariable=guiout)
+ent1.place(x=0,y=560)
+#buttons
+
+#text
+
+#keybinds
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
