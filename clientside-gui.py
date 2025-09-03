87f852686ac37bffc2dbcffe04ecbd3866340ea7
luke <125353302+lukeduck321@users.noreply.github.com>
2025-09-03 15:19:17 +0930

    Add files via upload

--- clientside-gui.py
+++ clientside-gui.py
@@ -2,14 +2,17 @@ import socket
 import threading
 from tkinter import *
 
+
 host = "0.0.0.0"
 port = 12345
-server_ip = "192.168.8.112"
-message = str()
+server_ip = "10.125.116.216"
+message = "example \nnew line\nnew line\nnew line\nnew line"
+out = str()
 
 #gui setup
 window = Tk()
 guiout = StringVar()
+l1 = Label(window, text=message)
 
 
 
@@ -19,19 +22,23 @@ def click():
     print(guiout.get())
     #send(server_ip,message)
     e1.delete(0,END)
-    send(server_ip,"//es")
-
+    #send(server_ip,"//es")
+        
 def enter(event):
     click()
 
 def refreshtwo():
-    print("passed r")
+    l1.destroy()
+    l1 = Label(window, text=message)
+    l1.grid(row = 0, column = 0,pady=25)
+    print("refreshed")
     #send(server_ip,"//es")
 
 def refresh(event):
     refreshtwo()
 
 def send(server_ip, message):
+    global out
     port = 12345
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -67,6 +74,7 @@ def rec():
             if not data:
                 break
             message = data.decode()
+            out =+ "\n" + data.decode()
             print("Received:", message)
             conn.send(b"message received")
         conn.close()
@@ -85,28 +93,20 @@ while True:
 
 window.geometry("400x600+1520+480")
 
-#enrties
-e1 = Entry(window, textvariable=guiout,width=50)
-e1.place(x=0,y=0)
+
 #buttons
 b1 = Button(window, text="send",height=1,width=5,command=click)
-b1.place(x=400,y=0)
+b1.grid(row = 4, column = 1,)
 b2 = Button(window, text="refresh",height=1,width=5,command=refreshtwo)
-b2.place(x=480,y=0)
+b2.grid(row = 4, column = 2)
 #text
-
+l1.grid(row = 0, column = 0,pady=25)
+#enrties
+e1 = Entry(window, textvariable=guiout,width=50)
+e1.grid(row = 4, column = 0)
 #keybinds
 window.bind('<Return>',enter)
 window.bind('<r>',refresh)
 window.bind('<R>',refresh)
 
 window.mainloop()
-
-
-
-
-
-
-
-
-
