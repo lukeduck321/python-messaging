dd711148ecf78336e63ff4c326885172a17e01f0
luke <125353302+lukeduck321@users.noreply.github.com>
2025-09-15 14:10:26 +0930

    Add files via upload

--- deployable/clientside-gui.py
+++ deployable/clientside-gui.py
@@ -12,6 +12,7 @@ cheight = str()
 chat_log = "" 
 passn=True
 change_check=str()
+send_error = False
 
 
 # GUI setup
@@ -29,8 +30,9 @@ def click():
     global message, guiout
     message = guiout.get() 
     print("Sending:", message)
-    send(server_ip, message)  
+    send(server_ip, message)
     e1.delete(0, END)
+    refreshtwo()
 
 
 def enter(event):
@@ -43,7 +45,7 @@ def refresh(event):
     refreshtwo()
 
 def send(server_ip, message):
-    global out, chat_log
+    global out, chat_log, send_error
     port = 12345
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -61,8 +63,10 @@ def send(server_ip, message):
         print("From server:\n", decoded)
         if message.strip() == "//es" or message.strip() == "Ducky: //es":
             chat_log = decoded
+            send_error = False
     except Exception as e:
         print("Send error:", e)
+        send_error = True
     finally:
         client_socket.close()
 
@@ -86,12 +90,12 @@ def rec():
         conn.close()
 
 def update_window():
-    global message, cwidth, cheight, c1, chat_log, change_check
+    global message, cwidth, cheight, c1, chat_log, change_check#z, send_error
     cheight = window.winfo_height() - 50
     cwidth = window.winfo_width() - 15
     
 
-    if change_check != str(cwidth)+str(cheight):          
+    if change_check != str(cwidth)+str(cheight):
         # Buttons
         b1 = Button(window, text="send", height=1, width=5, command=click)
         b1.grid(row=1, column=1,padx=5,pady=5)
@@ -106,11 +110,15 @@ def update_window():
     else:
         pass
     change_check= str(cwidth)+str(cheight)
-
     
+    error_message = "server not found\n\ncommon issues:\nthe server may currently be offline \nthe server ip in your app is incorrect\nyou may not be connected to the name network as the server\n\n please see your administrator"
     c1 = Canvas(window, width=cwidth, height=cheight-35)
-    c1.place(x=0,y=0)
-    c1.create_text(5, 5, anchor="nw", text=chat_log, fill="black", font=("Courier", 10), width=cwidth - 10)
+    c1.place(x=15,y=15)
+    if send_error == True:
+            c1.create_text(5, 5, anchor="nw", text=error_message, fill="black", font=("Courier", 10), width=cwidth - 10)
+    else:
+        c1.create_text(5, 5, anchor="nw", text=chat_log, fill="black", font=("Courier", 10), width=cwidth - 10)
+
     window.after(1000, update_window)
 
 b1 = Button(window, text="send", height=1, width=5, command=click)
