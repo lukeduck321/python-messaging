62bc8746cabfdfcf1d6057037737ef296d858654
luke <125353302+lukeduck321@users.noreply.github.com>
2025-09-17 15:18:09 +0930

    Add files via upload

--- Mandarin build/clientside-gui.py
+++ Mandarin build/clientside-gui.py
@@ -7,8 +7,6 @@ import account
 
 host = "0.0.0.0"
 port = 12345
-server_ip = str()
-username =str()
 message = "example \nnew line\nnew line\nnew line\nnew line"
 out = str()
 cwidth = str()
@@ -18,11 +16,17 @@ passn=True
 change_check=str()
 send_error = False
 
-splash_screen.start_load()
+
 account.account_setup()
+sleep(0.5)
+splash_screen.start_load()
 
 # GUI setup
 window = Tk()
+
+server_ip = StringVar()
+username =StringVar()
+
 guiout = StringVar()
 window.geometry("600x800+1520+480")
 
@@ -41,7 +45,6 @@ try:
             server_ip.set(lines[1])
 except FileNotFoundError:
     pass 
-
 
 def click():
     global message, guiout
@@ -63,6 +66,9 @@ def refresh(event):
 
 def send(server_ip, message):
     global out, chat_log, send_error
+
+    server_ip = str(server_ip)
+    
     port = 12345
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -130,7 +136,7 @@ def update_window():
         pass
     change_check= str(cwidth)+str(cheight)
     
-    error_message = "server not found\n\ncommon issues:\nthe server may currently be offline \nthe server ip in your app is incorrect\nyou may not be connected to the name network as the server\n\n please see your administrator"
+    error_message = "server not found\n\ncommon issues:\nthe server may currently be offline \nthe server ip in your app is incorrect\nyou may not be connected to the name network as the server\n\nplease see your administrator"
     c1 = Canvas(window, width=cwidth, height=cheight-35)
     c1.place(x=15,y=15)
     if send_error == True:
